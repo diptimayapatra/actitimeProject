ed8f603c76e0b5fb2c6476078dde095159829740
dipti <dipti@Diptimaya>
2023-07-27 18:43:25 +0530

    commit1

---
+++ MyExpBatch/src/com/actitime/testscripts/DeleteProject.java
@@ -0,0 +1,11 @@
+package com.actitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DeleteProject {
+     @Test
+     public void delete() {
+    	 Reporter.log("delete",true);
+     }
+}
